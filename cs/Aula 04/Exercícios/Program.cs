5311d4ca3f29c00bade3766036b3bce242bc767e
geucimar <geucimar@gmail.com>
2014-10-21 22:35:51 -0200

    Resolução exercício 10.

--- cs/Aula 04/Exercícios/Program.cs
+++ cs/Aula 04/Exercícios/Program.cs
@@ -20,10 +20,53 @@ namespace Exercícios
             //ExecutarExercicio6();
             //ExecutarExercicio7();
             //ExecutarExercicio8();
-            ExecutarExercicio9();
+            //ExecutarExercicio9();
+            ExecutarExercicio10();
 
 
             Console.ReadLine(); //faz uma pausa.
+        }
+
+
+        /**
+         * 10. Faça um algoritmo que leia um 
+         * número de 1 a 5 e escreva-o por extenso. 
+         * Caso o usuário digite um número 
+         * que não esteja neste intervalo, 
+         * exibir mensagem: número inválido.
+         **/
+        private static void ExecutarExercicio10()
+        {
+            Console.WriteLine("Informe um número:");
+            string s = Console.ReadLine();
+
+            int numeroDigitado = int.Parse(s);
+            string[] nomes = { "um", "dois", "três", "quatro", "cinco" };
+
+            //int[] numeros = new int[5];
+            int[] numeros = {1,2,3,4,5};
+
+            bool isNumeroValido = false;
+            
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                int valorDaPosicao = numeros[i];
+
+                string nome = nomes[i];
+               
+
+                if (numeroDigitado == valorDaPosicao)
+                {
+                    Console.WriteLine("O número digitado é {0}", nome);
+                    isNumeroValido = true;
+                }                
+            }
+
+            if (!isNumeroValido)
+            {
+                Console.WriteLine("O número é inválido!");
+            }
+
         }
 
         /**
